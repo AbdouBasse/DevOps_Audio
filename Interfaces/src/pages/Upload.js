a801c9b1a90cd215cfc691107988046784279af0
AbdouBasse <152453149+AbdouBasse@users.noreply.github.com>
2025-11-30 13:28:27 +0100

    Create Upload.js

---
+++ Interfaces/src/pages/Upload.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Upload from "../components/Upload";
+
+function UploadPage() {
+  return (
+    <div>
+      <h1>Upload Audio</h1>
+      <Upload />
+    </div>
+  );
+}
+
+export default UploadPage;
