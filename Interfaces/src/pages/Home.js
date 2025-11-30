794b5f6e676a9e5f4e5990c046adbc9273f486ff
AbdouBasse <152453149+AbdouBasse@users.noreply.github.com>
2025-11-30 13:27:35 +0100

    Create Home.js

---
+++ Interfaces/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur AudioOps Interface</h1>
+      <p>Choisissez une action : Upload, Mastering, Monitoring.</p>
+    </div>
+  );
+}
+
+export default Home;
